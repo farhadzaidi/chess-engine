b6c5bc0e856f180fe37ab52c1838b999592c7865
Syed Zaidi <zaidi.farhad03@gmail.com>
2024-03-21 15:24:57 -0400

    removed some commented out code

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,7 @@
 
 int main(int argc, char* argv[]) {
 	// Initialize board
-	// std::string FEN = "8/8/8/2q3k1/8/8/8/4K3 w - - 0 1";
-	// std::string FEN = "6k1/8/8/7R/5P2/8/8/7K w - - 0 1";
 	std::string FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-	// std::string FEN = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
 	Board b;
 	load_from_FEN(b, FEN);
 
